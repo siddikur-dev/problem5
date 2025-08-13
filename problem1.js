9105bc3fb47a6891c91f86227fd8002cb3385cb6
siddikur-dev <siddikur.dev@gmail.com>
2025-08-13 22:44:17 +0600

    problem 1 solved

---
+++ problem1.js
@@ -0,0 +1,12 @@
+// 💡Hints:  যদি ইনপুট 200  হয়, তাহলে fine হবে: 200 + 200 × ( 20 / 100) + 30 = 270 Taka
+
+function totalFine(fare) {
+  // You have to write your code here
+  if (typeof fare !== "number" || fare < 1) {
+    return "Invalid";
+  }
+
+  const fine = fare + fare * (20 / 100) + 30;
+  return fine;
+}
+console.log(totalFine("\Go"));
