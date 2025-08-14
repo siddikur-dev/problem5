83096f02b280528bdce674315fcb4d3d64ed6b9f
siddikur-dev <siddikur.dev@gmail.com>
2025-08-14 06:29:15 +0600

    problem 2 solved

--- problem2.js
+++ problem2.js
@@ -1,3 +1,11 @@
 function onlyCharacter(str) {
   // You have to write your code here
+  if (typeof str !== "string") {
+    return "Invalid";
+  }
+  let character = str.split(" ").join("").toUpperCase();
+  return character;
 }
+console.log(onlyCharacter(true
+
+));
