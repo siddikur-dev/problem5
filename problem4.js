8a30d468a0fb0f8c8bfb9e44ec1c1efeef2835e5
siddikur-dev <siddikur.dev@gmail.com>
2025-08-14 08:42:41 +0600

    problem 4 solved

--- problem4.js
+++ problem4.js
@@ -2,20 +2,15 @@ function isSame(arr1, arr2) {
   // You have to write your code here
   if (!Array.isArray(arr1) || !Array.isArray(arr2)) return "Invalid";
 
-  
+  if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
-    } else {
     }
   }
   return true;
 }
 
-const result = isSame(
-[2 , 5 , 6] ,256
-
-
-);
+const result = isSame([2, 5, 6], [2, 5, 6]);
 
 console.log(result);
